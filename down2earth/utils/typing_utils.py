4594d6d12364d00b55ca7056e5e5168058312486
Parsa Saberi <parsa.xmule@gmail.com>
2021-02-02 18:26:06 +0330

    create typing_utils

---
+++ down2earth/utils/typing_utils.py
@@ -0,0 +1,6 @@
+from typing import Union, Dict, Any
+
+NumericType = Union[int, float]
+StringMapping = Dict[str, str]
+JsonDictionary = Dict[str, Any]
+Json = Union[dict, list, int, float, str, bool]
